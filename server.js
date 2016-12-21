6bc7a1f263cf6e81dc312ba3a65beac438c2fba0
Michael Brassington <brasstrash@gmail.com>
2016-12-21 12:04:43 +0000

    add socket.io to client and server

--- server.js
+++ server.js
@@ -1,13 +1,15 @@
 var PORT = process.env.PORT || 3000;
 var express = require('express');
 var app = express();
-// Built in node module. Http server. Tells node to start a new server and to use this express app as the boilerplate
-var http = require('http').Server(app); 
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
-// expose a folder '/public'
-app.use(express.static(__dirname + '/public')); 
+app.use(express.static(__dirname + '/public'));
 
-// start the server
-http.listen(PORT, function () {
+io.on('connection', function() {
+	console.log('User connected via socket.io!')
+});
+
+http.listen(PORT, function() {
 	console.log('Server started');
 });
